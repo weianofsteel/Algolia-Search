cfc4859abfaaff11d352336eb9fe14e1ad41dec6
weianofsteel <skywalker0631@gmail.com>
2021-05-08 19:35:05 +0800

    edit config

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-  
 import App from 'next/app'
 import React from 'react';
 import Head from 'next/head'
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ServerStyleSheets } from '@material-ui/styles';
 import Document, {Html, Head, Main, NextScript} from 'next/document';
 
 class MyDocument extends Document {
@@ -44,18 +43,10 @@ class MyDocument extends Document {
 export default MyDocument;
 
 MyDocument.getInitialProps = async (ctx) => {
-    const sheets = new ServerStyleSheets();
-    const originalRenderPage = ctx.renderPage;
-  
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-      });
   
     const initialProps = await Document.getInitialProps(ctx);
   
     return {
-      ...initialProps,
-      styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+      ...initialProps
     };
   };
